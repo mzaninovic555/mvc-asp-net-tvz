f31518943458be74ab292d98a46259d04d84f5d7
Marko Zaninović <mzaninovic555@gmail.com>
2022-04-09 21:54:06 +0200

    Dodan kod

---
+++ README.md
@@ -0,0 +1,2 @@
+# mvc-2021-22
+Repozitorij za predmet ASP.NET MVC 2021/22
---
+++ Vjezba/Vjezba.Web/Mock/City.cs
@@ -0,0 +1,8 @@
+﻿namespace Vjezba.Web.Mock
+{
+    public class City
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
