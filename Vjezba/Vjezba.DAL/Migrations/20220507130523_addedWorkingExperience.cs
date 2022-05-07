0b3177de6977db62ac7da5c2d78eb66e9bf38f4c
Marko Zaninović <mzaninovic555@gmail.com>
2022-05-07 15:11:01 +0200

    Zadatak 4.3: dodan WorkingExperience, migracija, dodane validacije

---
+++ Vjezba/Vjezba.DAL/Migrations/20220507130523_addedWorkingExperience.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vjezba.DAL.Migrations
+{
+    public partial class addedWorkingExperience : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "WorkingExperience",
+                table: "Clients",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WorkingExperience",
+                table: "Clients");
+        }
+    }
+}
--- Vjezba/Vjezba.Model/Client.cs
+++ Vjezba/Vjezba.Model/Client.cs
@@ -7,12 +7,21 @@ namespace Vjezba.Model
     {
         [Key]
         public int ID { get; set; }
+
+        [StringLength(30)]
         public string FirstName { get; set; }
+
+        [StringLength(30)]
         public string LastName { get; set; }
+
+        [StringLength(30, ErrorMessage = "Maximum length allowed is 30 characters")]
         public string Email { get; set; }
         public char Gender { get; set; }
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Working experience has to be within 0-100")]
+        public int? WorkingExperience { get; set; }
 
         [ForeignKey(nameof(City))]
         public int? CityID { get; set; }
