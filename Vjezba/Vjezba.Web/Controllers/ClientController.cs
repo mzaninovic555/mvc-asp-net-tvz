aee5bec15afbf5157ca58bed1ad68337b70f7315
Marko Zaninović <mzaninovic555@gmail.com>
2022-05-22 11:56:02 +0200

    Clean up nekorištenih namespace-ova.

--- Vjezba/Vjezba.Web/Controllers/ClientController.cs
+++ Vjezba/Vjezba.Web/Controllers/ClientController.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Vjezba.DAL;
 using Vjezba.Model;
 using Vjezba.Web.Models;
--- Vjezba/Vjezba.Web/Mock/City.cs
+++ Vjezba/Vjezba.Web/Mock/City.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Vjezba.Web.Mock
+﻿namespace Vjezba.Web.Mock
 {
     public class City
     {
